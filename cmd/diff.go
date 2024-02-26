d0617ddef885d99a72bf68df7336a88e39c62f0a
Scott Richardson <scottjr632@gmail.com>
2024-02-25 22:39:42 -0800

    add ability to diff previous branch

---
+++ cmd/diff.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/scottjr632/sequoia/internal/cli"
+	"github.com/scottjr632/sequoia/internal/engine"
+	"github.com/spf13/cobra"
+)
+
+var diffCmd = &cobra.Command{
+	Use:   "diff",
+	Short: "Show changes between parent and this branch",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		stack, err := engine.GetStackForCurrentBranch()
+		if err != nil {
+			return err
+		}
+		parentStack := stack.GetParent()
+		return cli.ExecuteCommandInTerminal("git", "diff", parentStack.Name)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(diffCmd)
+}
