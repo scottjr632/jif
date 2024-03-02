2b33fac065153586f11e04465fa875ef3f9afb53
Scott Richardson <scottjr632@gmail.com>
2024-03-02 12:36:10 -0800

    Add restack command

---
+++ cmd/restack.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/scottjr632/sequoia/internal/engine"
+	"github.com/spf13/cobra"
+)
+
+var restackCmd = &cobra.Command{
+	Use:   "restack",
+	Short: "Restack the branches in the stack",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		stack, err := engine.GetStackForCurrentBranch()
+		if err != nil {
+			return err
+		}
+		return engine.RestackChildren(stack)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(restackCmd)
+}
